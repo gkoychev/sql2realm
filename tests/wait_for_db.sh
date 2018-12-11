25c1f7fdde19fc17c8b90bf2ba0d6bf21c999d68
Georgy Koychev <kgm.pochta@gmail.com>
2018-12-11 21:55:46 +0200

    fix travis 8

--- tests/wait_for_db.sh
+++ tests/wait_for_db.sh
@@ -2,12 +2,16 @@
 # wait until MySQL is really available
 maxcounter=45
  
+echo "$MYSQL_USER"
+echo "$MYSQL_PASSWORD"
+
 counter=1
-while ! mysql --protocol TCP -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "show databases;" > /dev/null 2>&1; do
-    sleep 1
+while ! mysql --protocol TCP -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "show databases;"; do
     counter=`expr $counter + 1`
     if [ $counter -gt $maxcounter ]; then
         >&2 echo "We have been waiting for MySQL too long already; failing."
         exit 1
     fi;
+    echo "$counter"
+    sleep 1
 done
