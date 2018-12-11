366f963909abb91ad2d201001e2802dcfbd801dd
Georgy Koychev <kgm.pochta@gmail.com>
2018-12-11 22:26:35 +0200

    try travis 12

--- tests/wait_for_db.sh
+++ tests/wait_for_db.sh
@@ -6,7 +6,7 @@ MYSQL_USER=test
 MYSQL_PASSWORD=test
 
 counter=1
-while ! mysql --protocol TCP -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "show databases;"; do
+while ! mysql --protocol TCP -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "show databases;" > /dev/null 2>&1; do
     counter=`expr $counter + 1`
     if [ $counter -gt $maxcounter ]; then
         >&2 echo "We have been waiting for MySQL too long already; failing."
