cfc7d54be6f91e8b7609cd2b09b290d58eb143f9
Georgy Koychev <kgm.pochta@gmail.com>
2018-12-12 11:20:12 +0200

    test 3

--- tests/wait_for_db.sh
+++ tests/wait_for_db.sh
@@ -2,11 +2,8 @@
 # wait until MySQL is really available
 maxcounter=30
  
-echo $MYSQL_USER
-echo $MYSQL_PASSWORD
-
-MYSQL_USER=test
-MYSQL_PASSWORD=test
+# echo $MYSQL_USER
+# echo $MYSQL_PASSWORD
 
 counter=1
 while ! mysql --protocol TCP -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "show databases;" > /dev/null 2>&1; do
@@ -15,5 +12,6 @@ while ! mysql --protocol TCP -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "show databa
         >&2 echo "We have been waiting for MySQL too long already; failing."
         exit 1
     fi;
+    echo "waiting for DB $counter"
     sleep 2
 done
